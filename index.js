51a058d869c805bb2a4e13e2cc31d2d634e23d7b
FarrelAN <m.farrel@ui.ac.id>
2024-06-25 01:03:01 +0700

    test 1

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ import messageRouter from "./routes/messageRoutes.js";
 const app = express();
 dotenv.config();
 
-app.use(cors());
+const cors = require("cors");
+
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://farrelan.vercel.app"],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
